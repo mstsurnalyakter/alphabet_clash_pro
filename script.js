aed049a42a00acbdfcc01b6734223bba0a15e723
mstsurnalyakter <surnalyms@gmail.com>
2024-02-14 08:23:36 +0600

    alphabet background set

--- script.js
+++ script.js
@@ -10,17 +10,34 @@ const showElementById = id =>{
     getElementById(id).classList.remove("hidden");
 }
 
+const setBackgroundColorById = id =>{
+    const element = getElementById(id);
+    element.classList.add("bg-orange-400");
+}
+
 const getARandomAlphabet = () =>{
     const alphabetString = "abcdefghijklmnopqrstuvwxyz";
     const alphabets = alphabetString.split("");
+
     const index = Math.round(Math.random()*25);
     const alphabet = alphabets[index];
+
     return alphabet;
 }
 
 
 const continueGame = () => {
-  const alphabet = getARandomAlphabet()
+    //step - 1: generate a random alphabet
+  const alphabet = getARandomAlphabet();
+
+  //step - 2: set randomly generate alphabet
+  const para = getElementById("current-alphabet");
+  para.innerText = alphabet;
+
+  //step - 3 set background color
+  setBackgroundColorById(alphabet);
+
+
 };
 
 getElementById("homeBtn").addEventListener('click',()=>{
