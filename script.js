392f123ebe672ab40dea1d604a83d207cc9283a6
mstsurnalyakter <surnalyms@gmail.com>
2024-02-14 07:55:52 +0600

    random alphabet generate

--- script.js
+++ script.js
@@ -10,7 +10,21 @@ const showElementById = id =>{
     getElementById(id).classList.remove("hidden");
 }
 
+const getARandomAlphabet = () =>{
+    const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+    const alphabets = alphabetString.split("");
+    const index = Math.round(Math.random()*25);
+    const alphabet = alphabets[index];
+    return alphabet;
+}
+
+
+const continueGame = () => {
+  const alphabet = getARandomAlphabet()
+};
+
 getElementById("homeBtn").addEventListener('click',()=>{
     hiddenElementById("home-section");
     showElementById("play-ground-section");
+    continueGame();
 });
