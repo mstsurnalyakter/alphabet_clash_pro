d77edbfa98c06ec272fe1f169dd48e4196480354
mstsurnalyakter <surnalyms@gmail.com>
2024-02-19 19:18:02 +0600

    added audio

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// common function start
 const getElementById = (btnId) => {
   return document.getElementById(btnId);
 };
@@ -9,6 +10,16 @@ const addClass = (id, className) => {
 const removeClass = (id, className) => {
   getElementById(id).classList.remove(className);
 };
+
+const getScore = (id) => {
+  return parseInt(getElementById(id).innerText);
+};
+
+const updateScore = (id, score) => {
+  getElementById(id).innerText = score;
+};
+
+// common function end
 
 const getARandomAlphabet = () => {
   const alphabetString = "abcdefghijklmnopqrstuvwxyz";
@@ -22,40 +33,37 @@ const getARandomAlphabet = () => {
 
 document.addEventListener("keyup", (e) => {
   const playPressedAlphabet = e.key.toLowerCase();
+
   const expectedAlphabet =
     getElementById("current-alphabet").innerText.toLowerCase();
 
-  let currentScore = parseInt(getElementById("score").innerText);
-  let lifeScore = parseInt(getElementById("life").innerText);
-
+  let currentScore = getScore("score");
+  let lifeScore = getScore("life");
   if (playPressedAlphabet === expectedAlphabet) {
     //update score
     currentScore++;
-    getElementById("score").innerText = currentScore;
-
+    updateScore("score", currentScore);
     //start a new ground
     removeClass(expectedAlphabet, "bg-orange-400");
     continueGame();
   } else {
     lifeScore--;
-    getElementById("life").innerText = lifeScore;
+    updateScore("life", lifeScore);
+
     removeClass(expectedAlphabet, "bg-orange-400");
     continueGame();
 
-    if (lifeScore <= 0) {
-      addClass("play-ground-section", "hidden");
-      removeClass("score-section", "hidden");
-      getElementById("gain-score").innerText = currentScore;
-
-      getElementById("play-again").addEventListener("click", (e) => {
-        removeClass("play-ground-section", "hidden");
-        addClass("score-section", "hidden");
-        getElementById("score").innerText = 0;
-        getElementById("life").innerText = 5;
-      });
+    if (lifeScore === 0) {
+      gameOver(currentScore);
     }
   }
+
+  // stop the game if pressed 'Esc'
+  if (playPressedAlphabet === "escape") {
+    gameOver(currentScore);
+  }
 });
+
 const continueGame = () => {
   //step - 1: generate a random alphabet
   const alphabet = getARandomAlphabet();
@@ -73,3 +81,19 @@ getElementById("homeBtn").addEventListener("click", () => {
   removeClass("play-ground-section", "hidden");
   continueGame();
 });
+
+const gameOver = (currentScore) => {
+  addClass("play-ground-section", "hidden");
+  removeClass("score-section", "hidden");
+   updateScore("gain-score", currentScore);
+   playAgain();
+};
+
+const playAgain = () =>{
+   getElementById("play-again").addEventListener("click", (e) => {
+     removeClass("play-ground-section", "hidden");
+     addClass("score-section", "hidden");
+     updateScore("score",0);
+     updateScore("life", 5)
+   });
+}
